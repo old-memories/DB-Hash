edcfd94336c94185dd1a97c55aff4262ef52bfef
张子扬 <zhangziyang.1210@bytedance.com>
2021-04-30 11:59:14 +0800

    only string hash func

--- include/hash_func.h
+++ include/hash_func.h
@@ -2,11 +2,11 @@
 #define __HASH_FUNC_H__
 
 #include <stddef.h>
+#include <string.h>
 
 #define PRIME_NUMBERS 25
 
 typedef enum hash_type {
-    HASH_NUM,
     HASH_STRING,
     HASH_TYPES
 } hash_type_t;
@@ -15,11 +15,12 @@ typedef struct hash_func_info {
     char *name;
     hash_type_t type;
     size_t (*hash_func)(const void *key, size_t key_len, size_t size);
+    int (*key_cmp)(const void *k1, const void *k2, size_t key_len);
 } hash_func_info_t;
 
-extern const size_t prime_numbers[PRIME_NUMBERS];
-extern size_t round_to_prime_number(size_t number);
-extern size_t hash_func_number(const void *key, size_t key_len, size_t size);
 extern size_t hash_func_string(const void *key, size_t key_len, size_t size);
+extern int key_string_cmp(const void *k1, const void *k2, size_t key_len);
+extern size_t round_to_prime_number(size_t number);
+
 
 #endif
